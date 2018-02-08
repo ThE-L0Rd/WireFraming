84aef3405fafafee08c3718efc2d9a618a911a59
Unknown <khaled.ja5mg@gmail.com>
2018-02-08 22:27:57 +0200

    s
    s

---
+++ t2de.py
@@ -0,0 +1,19 @@
+
+def Atm(request):
+
+  giveString = 'give :'
+  validPapers = [100, 50, 10, 5]
+  validPaperIndex = 0
+
+  while request > 5:
+
+      while request > validPapers[validPaperIndex]:
+          print(giveString + str(validPapers[validPaperIndex]))
+          request -= validPapers[validPaperIndex]
+
+      validPaperIndex += 1
+
+  return(giveString + str(request))
+
+
+print(Atm(277))
