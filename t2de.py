f5443967bd91df9dc0d48bdaa4e621696316e864
Unknown <khaled.ja5mg@gmail.com>
2018-02-10 20:13:24 +0200

    f

--- t2de.py
+++ t2de.py
@@ -1,20 +1,30 @@
-def Atm(request):
+def withdraw(balance,request):
+    delet = request
+    if balance == 0:
+        print "You don't have any papers :|"
+    elif balance < request:
+        print "You don't have enough papers :( "
+    giveString = 'give :'
+    validPapers = [100, 50, 10, 5]
+    validPaperIndex = 0
+    if request =>50 and request <=99 :
+        validPaperIndex = 1
+    elif request =>5 and request <=49 :
+        validPaperIndex = 1
+    if request >= 5:
+        while request >= 5:
 
-  giveString = 'give :'
-  validPapers = [100, 50, 10, 5]
-  validPaperIndex = 0
+            while request > validPapers[validPaperIndex]:
+                print(giveString + str(validPapers[validPaperIndex]))
+                request -= validPapers[validPaperIndex]
 
-  while request > 5:
+            validPaperIndex += 1
 
-      while request > validPapers[validPaperIndex]:
-          print(giveString + str(validPapers[validPaperIndex]))
-          request -= validPapers[validPaperIndex]
+        print (giveString + str(request))
+    return balance - delet
 
-      validPaperIndex += 1
-
-  return(giveString + str(request))
-
-
-print(Atm(277))
-
-
+balance = 500
+balance = withdraw(balance,277)
+balance = withdraw(balance, 30)
+balance = withdraw(balance, 5)
+balance = withdraw(balance, 500)
