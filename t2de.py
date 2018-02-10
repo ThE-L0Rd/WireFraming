b73fde6cc8fca49d906a6d34c6da21a6bb713843
Unknown <khaled.ja5mg@gmail.com>
2018-02-10 15:17:08 +0200

    atm v 1
    atm v 1

--- t2de.py
+++ t2de.py
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 def Atm(request):
 
   giveString = 'give :'
@@ -18,16 +16,5 @@ def Atm(request):
 
 
 print(Atm(277))
-=======
-def Atm(n):
-    g = 'give :'
-    l = [100,50,10,5]
-    nu= 0
-    while n >5 :
-        while n > l[nu]:
-            print g + str(l[nu])
-            n -= l[nu]
-        nu +=1
-    return g +str(n)
-print Atm(277)
->>>>>>> master
+
+
